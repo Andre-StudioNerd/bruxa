fbc779a8b62abfa9918c3a81d445d2848aedf317
André Luis Fernandes <deco.luisfernandes@gmail.com>
2020-06-26 16:53:29 -0300

    uo

--- scripts/load.js
+++ scripts/load.js
@@ -6,7 +6,7 @@ function preload() {
   fonteTelaInicial =
     loadFont('imagens/assets/fonteTelaInicial.otf');
   imagemPersonagem =
-    loadImage('imagens/personagem/seiya.png');
+    loadImage('imagens/personagem/seiya2.png');
   imagemInimigo =
     loadImage('imagens/inimigos/gotinha.png');
   imagemInimigoGrande =
